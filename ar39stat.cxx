d26e4747e47bd032a460a12ed2246938c105f226
Katharina von Sturm <katharinavonsturm@gmail.com>
2021-05-13 12:39:06 +0200

    [teststat] add Chi2 calculated by hand normalize model to integral in data

--- ar39stat.cxx
+++ ar39stat.cxx
@@ -28,6 +28,7 @@
 #include "TCanvas.h"
 #include "TGraph2D.h"
 #include "TRandom2.h"
+#include "TParameter.h"
 #include "TStyle.h"
 
 // json
@@ -85,6 +86,7 @@ std::string get_filename(int fccd, double dlf);
 std::string get_ofilename(bool toys, bool interpolate, int channel, int rebin, range_t r);
 std::string get_treename(int channel, range_t r);
 void scale_TH1D_to_integral(TH1D * h, range_t range);
+double GetChi2(TH1D * h_data, TH1D * h_model, range_t range);
 
 int main(int argc, char* argv[]) {
 
@@ -104,7 +106,7 @@ int main(int argc, char* argv[]) {
   uint16_t channel = 0;
 
   uint16_t rebin = 1;
-  uint16_t teststat = 0;
+  uint16_t teststat = 5;
   bool interpolate = false;
   range_t fit_range(10000, 45, 160);
 
@@ -205,14 +207,17 @@ int main(int argc, char* argv[]) {
   if (verbose) {
     if (toys) std::cout << "Processing: gerda-factory toy MCs - " << infile << std::endl;
     else      std::cout << "Processing: Real Data - " << infile << std::endl;
-    std::cout << "channel : " << channel        << "\n";
-    std::cout << "rebin : "   << rebin          << "\n";
-    std::cout << "range   : " << fit_range.emin << " - " << fit_range.emax  << " keV\n";
+    std::cout << "channel: " << channel << "\n";
+    std::cout << "rebin: " << rebin << "\n";
+    std::cout << "range: " << fit_range.emin << " - " << fit_range.emax  << " keV\n";
     std::cout << "test statistics: "; 
     switch (teststat) {
-      case 0  : std::cout << "delta Chi2\n";     break;
-      case 1  : std::cout << "Chi2Test\n";       break;
-      case 2  : std::cout << "KolmogorovTest\n"; break;
+      case 0  : std::cout << "Chi2Test\n";             break;
+      case 1  : std::cout << "delta Chi2Test\n";       break;
+      case 2  : std::cout << "KolmogorovTest\n";       break;
+      case 3  : std::cout << "delta KolmogorovTest\n"; break;
+      case 4  : std::cout << "Chi2\n";                 break;
+      case 5  : std::cout << "delta Chi2\n";           break;
       default : std::cout << "Test statistics not implemented using default: delta Chi2\n"; break;
     }
     if (!interpolate)  std::cout << "DO NOT ";
@@ -240,11 +245,13 @@ int main(int argc, char* argv[]) {
       auto key = (TKey*) keyAsObj;
       if (std::string(key->GetClassName()) == "TH1D") {
         v_data[hct] = dynamic_cast<TH1D*>( fin.Get(key->GetName()) );
-        for (int b = 1; b <= v_data[hct]->GetNbinsX(); b++) {
-          double cont = v_data[hct]->GetBinContent(b);
-          v_data[hct]->SetBinContent(b,0);
-          for (int i = 0; i < cont; i++)
-            v_data[hct]->Fill(v_data[hct]->GetBinCenter(b));
+        if (teststat < 4) { // make sure histogram is unweighted
+          for (int b = 1; b <= v_data[hct]->GetNbinsX(); b++) {
+            double cont = v_data[hct]->GetBinContent(b);
+            v_data[hct]->SetBinContent(b,0);
+            for (int i = 0; i < cont; i++)
+              v_data[hct]->Fill(v_data[hct]->GetBinCenter(b));
+          }
         }
         hct++;
       }
@@ -252,7 +259,7 @@ int main(int argc, char* argv[]) {
     v_data.resize(hct);
   }
   fin.Close();
-  
+
   // load model histograms 
   for (auto && m : models) {
     std::string mname = Form("model_fccd%d_dlf%03d",m.fccd,(int)round(m.dlf*100));
@@ -273,21 +280,42 @@ int main(int argc, char* argv[]) {
       int nbins = m.hist->GetNbinsX();
       for (int b = 1; b <= nbins; b++) {
         double bin_center = m.hist->GetBinCenter(b);
-        if (fit_range.emin-10. <= bin_center && bin_center <= fit_range.emax+10.) {
+        if (0. <= bin_center && bin_center <= 500.) {
           double cont = gerda::ar39_pdf(channel, bin_center, m.fccd/1000., m.dlf);
           m.hist->SetBinContent(b,cont);
         }
         m.hist->SetBinError(b,0.);
       }
-      auto bin_emin = m.hist->FindBin(fit_range.emin);
-      auto bin_emax = m.hist->FindBin(fit_range.emax);
-      double integral = m.hist->Integral(bin_emin,bin_emax);
-
-      double scale = 1000.*(fit_range.emax-fit_range.emin)/(m.hist->GetBinWidth(1)*rebin)/integral;
-      m.hist->Scale(scale);
+      if (teststat < 4) m.hist->Scale(4.e10);
     }
     m.hist->Rebin(rebin);
     m.chi2.resize(v_data.size());
+  }
+
+  // Write model histograms
+  if (interpolate) {
+    TFile ofh((outdir+"/models_"+prefix+get_ofilename(toys,interpolate,channel,rebin,fit_range)).c_str(),"RECREATE");
+    TCanvas * cm_fccd = new TCanvas("cm_fccd","cm_fccd",1000,700);
+    TCanvas * cm_dlf  = new TCanvas("cm_dlf","cm_dlf",1000,700);
+    int i = 0, j = 0;
+    for (auto && m : models) {
+      if (round(m.dlf*100) == 60) { 
+        cm_fccd->cd();
+        m.hist->SetLineColor( i%2==0 ? kRed : kBlue );
+        m.hist->DrawCopy(i == 0 ? "hist" : "hist same");
+        i++;
+      }
+      if (m.fccd == 1750) {
+        cm_dlf->cd();
+        m.hist->SetLineColor( j%2==0 ? kRed : kBlue );
+        m.hist->DrawCopy(j == 0 ? "hist" : "hist same");
+        j++;
+      }
+      //ofh.cd(); m.hist->Write();
+    }
+    cm_fccd->Write();
+    cm_dlf->Write();
+    ofh.Close();
   }
 
   progressbar bar(toys ? v_data.size() : models.size());
@@ -329,18 +357,24 @@ int main(int argc, char* argv[]) {
       m.hist->GetXaxis()->SetRangeUser(fit_range.emin,fit_range.emax);
       data  ->GetXaxis()->SetRangeUser(fit_range.emin,fit_range.emax);
 
-      if (data->Integral(data->FindBin(fit_range.emin),data->FindBin(fit_range.emax)) < 10) {
-        std::cout << "\nEmpty Hist: " << data->GetName() << " " << data->Integral() << std::endl;
-        exit(EXIT_FAILURE);
-      }
-      if (m.hist->Integral(m.hist->FindBin(fit_range.emin),m.hist->FindBin(fit_range.emax)) < 10) {
-        std::cout << "\nEmpty Hist: " << m.hist->GetName() << " " << m.hist->Integral() << std::endl;
-        exit(EXIT_FAILURE);
+      if (teststat < 4) {
+        if (data->Integral(data->FindBin(fit_range.emin),data->FindBin(fit_range.emax)) < 10) {
+          std::cout << "\nEmpty Hist: " << data->GetName() << " " << data->Integral() << std::endl;
+          exit(EXIT_FAILURE);
+        }
+        if (m.hist->Integral(m.hist->FindBin(fit_range.emin),m.hist->FindBin(fit_range.emax)) < 10) {
+          std::cout << "\nEmpty Hist: " << m.hist->GetName() << " " << m.hist->Integral() << std::endl;
+          exit(EXIT_FAILURE);
+        }
       }
 
       switch (teststat) {
-        case 2  : m.chi2.at(i) = data->KolmogorovTest(m.hist);      break;
-        default : m.chi2.at(i) = data->Chi2Test(m.hist, "UW CHI2"); break;
+        case 0  : m.chi2.at(i) = data->Chi2Test(m.hist, "UW CHI2"); break; // Chi2
+        case 1  : m.chi2.at(i) = data->Chi2Test(m.hist, "UW CHI2"); break; // Chi2 delta
+        case 2  : m.chi2.at(i) = data->KolmogorovTest(m.hist);      break; // KolmogorovTest
+        case 3  : m.chi2.at(i) = data->KolmogorovTest(m.hist);      break; // KolmogorovTest delta
+        case 4  : m.chi2.at(i) = GetChi2(data,m.hist,fit_range); break; // Chi2 by-hand
+        default : m.chi2.at(i) = GetChi2(data,m.hist,fit_range); teststat = 5; break; // Chi2 by-hand delta
       }
     }
     i++;
@@ -365,18 +399,19 @@ int main(int argc, char* argv[]) {
     // find minimum
     min_llh = std::min_element(std::begin(v_chi2),std::end(v_chi2));
     // delta chi2
-    if (teststat == 0 || teststat > 2) {
+    if (teststat%2 == 0) {
       std::transform(std::begin(v_chi2), std::end(v_chi2), std::begin(v_chi2), [c=*min_llh](double x) { return x -= c; } );
     }
     // minimum corrisponds to best fit
     best_fccd = models.at(min_llh-std::begin(v_chi2)).fccd;
-    best_dlf = models.at(min_llh-std::begin(v_chi2)).dlf;
+    best_dlf  = models.at(min_llh-std::begin(v_chi2)).dlf;
     tree.Fill();
   }
 
   tree.Write();
   of.Close();
 
+  // some useful output
   if (!toys) {
     TFile ofh((outdir+"/bestfit_"+prefix+get_ofilename(toys,interpolate,channel,rebin,fit_range)).c_str(),"RECREATE");
     // Canvas with data, best model and range in DLF
@@ -411,6 +446,11 @@ int main(int argc, char* argv[]) {
       hm->Draw("hist same");
     }
     c2->Write("best_fit_fccd");
+
+    TParameter<int>    p_best_fccd("best_fccd", best_fccd);
+    TParameter<double> p_best_dlf ("best_dlf",  best_dlf);
+    p_best_fccd.Write();
+    p_best_dlf.Write();
 
     // Canvas with TS profile
     TH2D * ts_profile = new TH2D("ts_profile","ts_profile",
@@ -466,4 +506,25 @@ std::string get_treename(int channel, range_t r) {
 
 void scale_TH1D_to_integral(TH1D * h, range_t range) {
   h->Scale(1./h->Integral(h->FindBin(range.emin), h->FindBin(range.emax)));
+}
+
+double GetChi2(TH1D * h_data, TH1D * h_model, range_t range) {
+
+  int bmin = h_data->FindBin(range.emin);
+  int bmax = h_data->FindBin(range.emax);
+
+  double int_data  = h_data->Integral(bmin,bmax);
+  double int_model = h_model->Integral(bmin,bmax);
+
+  h_model->Scale(int_data/int_model);
+
+  double chi2 = 0.;
+
+  for (int b = bmin; b <= bmax; b++) {
+    double c_data  = h_data->GetBinContent(b);
+    double c_model = h_model->GetBinContent(b);
+    chi2 += (c_model - c_data) * (c_model - c_data) / c_model;
+  }
+
+  return chi2;
 }
